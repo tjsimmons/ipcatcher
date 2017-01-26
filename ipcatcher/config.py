47533c89ef4cb67b6e17dbec7ba3f3c2e9174241
T.J. Simmons <tj@tjsimmons.net>
2017-01-26 11:25:48 -0600

    Update config.py

--- ipcatcher/config.py
+++ ipcatcher/config.py
@@ -1,9 +1,9 @@
 DEBUG = False
-DB_NAME = 'dev'
-DB_USER = 'postgres'
-DB_PASS = 'postgres'
-DB_SERVICE = 'postgres'
-DB_PORT = '5342'
+DB_NAME = os.environ['DB_NAME']
+DB_USER = os.environ['DB_USER']
+DB_PASS = os.environ['DB_PASS']
+DB_SERVICE = os.environ['DB_SERVICE']
+DB_PORT = os.environ['DB_PORT']
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
     DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
 )
