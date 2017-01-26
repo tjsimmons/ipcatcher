d1812b6c2397996bcf2c76d8b2b5e6cb6c81d0a2
T.J. Simmons <tj@tjsimmons.net>
2017-01-26 09:21:22 -0600

    switch to postgres. need to figure out adding a new user again

--- ipcatcher/config.py
+++ ipcatcher/config.py
@@ -1,5 +1,12 @@
 DEBUG = False
-SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
+DB_NAME = 'dev'
+DB_USER = 'postgres'
+DB_PASS = 'postgres'
+DB_SERVICE = 'postgres'
+DB_PORT = '5342'
+SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+    DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
+)
 
 try:
     from local_config import *
