b108075754fef806265b6fe0ddb8107029f264ed
T.J. Simmons <tj@tjsimmons.net>
2017-01-26 11:32:28 -0600

    No need for local_config if managing through env vars

--- ipcatcher/config.py
+++ ipcatcher/config.py
@@ -1,4 +1,4 @@
-DEBUG = False
+DEBUG = True
 DB_NAME = os.environ['DB_NAME']
 DB_USER = os.environ['DB_USER']
 DB_PASS = os.environ['DB_PASS']
@@ -7,8 +7,3 @@ DB_PORT = os.environ['DB_PORT']
 SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
     DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
 )
-
-try:
-    from local_config import *
-except:
-    pass
