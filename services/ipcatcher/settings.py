6646e50a5d3f407a9e7076485f64c54bc9217aea
T.J. Simmons <tj@tjsimmons.net>
2017-01-28 22:58:13 -0600

    Update settings.py

--- services/ipcatcher/settings.py
+++ services/ipcatcher/settings.py
@@ -80,7 +80,7 @@ if not DEBUG:
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': os.environ.get('DB_NAME'),
             'USER': os.environ.get('DB_USER'),
-            'PASSWORD': os.environ.get('DB_PASSWORD'),
+            'PASSWORD': os.environ.get('DB_PASS'),
             'HOST': os.environ.get('DB_HOST'),
             'PORT': os.environ.get('DB_PORT'),
         }
