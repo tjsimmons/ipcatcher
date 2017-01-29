7e15ea98d435413c8df8bceec3aa1748e9684168
T.J. Simmons <tj@tjsimmons.net>
2017-01-29 11:24:30 -0600

    fix real ip

--- services/services/views.py
+++ services/services/views.py
@@ -8,15 +8,15 @@ from .models import Home
 
 # Create your views here.
 @csrf_exempt
+@require_POST
 def index(request):
     key = request.POST.get('key', None)
-    print(key)
-    print('hey')
+
     if key is None:
         return HttpResponseBadRequest()
 
     home = get_object_or_404(Home, psk=key)
-    home.ip = request.META['REMOTE_ADDR']
+    home.ip = request.META['HTTP_X_REAL_IP']
     home.last_check_date = datetime.now()
 
     home.save()
