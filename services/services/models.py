446f3441b91c8549783448b7c6910abd15d0b978
T.J. Simmons <tj@tjsimmons.net>
2017-02-01 19:40:26 -0600

    helpful to have datetime when you want datetime

--- services/services/models.py
+++ services/services/models.py
@@ -6,7 +6,7 @@ class Home(models.Model):
     name = models.CharField(max_length=50)
     ip = models.CharField(max_length=50, null=True, blank=True)
     psk = models.CharField(max_length=128, unique=True)
-    last_check_date = models.DateField(null=True, blank=True)
+    last_check_date = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return "{0} - {1}".format(self.name, self.ip)
+        return "{0} - {1} - {2}".format(self.name, self.ip, self.last_check_date)
